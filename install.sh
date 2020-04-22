a9e0e19311f3af1659ac5361b65ccf4cd83a8a52
Jackson Millsaps <jmillsaps@datatrac.com>
2020-04-22 07:38:10 -0400

    mv dotfiles to .back before linking

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+mv  $HOME/.spacemacs $HOME/.spacemacs.back
+mv  $HOME/.tmux.conf $HOME/.tmux.conf.back
+mv  $HOME/.vim/vimrc $HOME/.vim/vimrc.back
+
 ln -s .spacemacs $HOME/.spacemacs
 ln -s .tmux.conf $HOME/.tmux.conf
 ln -s vimrc $HOME/.vim/vimrc
